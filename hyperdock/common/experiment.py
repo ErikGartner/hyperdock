d00eb04f2181022a207209121fb79e21c2d19cde
Erik Gärtner <erik@gartner.io>
2018-06-02 15:58:22 +0200

    Update with experiment class

---
+++ hyperdock/common/experiment.py
@@ -0,0 +1,75 @@
+
+
+class Experiment:
+    """
+    Class managing an experiment.
+    """
+
+    def __init__(self, job):
+        super().__init__()
+
+        self._queue_job = job
+        self.id = job['_id']
+
+    def start(self):
+        """
+        Starts the experiment.
+        """
+        raise NotImplementedError()
+
+    def stop(self):
+        """
+        Stops the experiment.
+        """
+        pass
+
+    def cleanup(self):
+        """
+        Stops and removes temporary files.
+        """
+        pass
+
+    def is_running(self):
+        """
+        Checks with underlying runner if the current experiment is running.
+        Otherwise it could: not be started, finished or failed.
+        """
+        pass
+
+    def get_result(self):
+        """
+        Returns the result, that is: state, loss, and any extra data.
+        """
+        pass
+
+    def __str__(self):
+        return 'Experiment: %s' % self._queue_job
+
+
+class MockExperiment(Experiment):
+    """
+    A mock experiment that doesn't actually run any experiment.
+    It runs for one check then finishes. Returns a loss of 1.0.
+    """
+
+    def __init__(self, job):
+        super().__init__(job)
+        self.running = False
+        self.failure = False
+
+    def start(self):
+        self.running = True
+        self.failure = False
+
+    def is_running(self):
+        r = self.running
+        self.running = False
+        return r
+
+    def stop(self):
+        self.running = False
+        self.failure = True
+
+    def get_result(self):
+        state = 'ok' if not self.failure else 'failure'
+        return {'loss': 1, 'state': state}
--- hyperdock/worker/worker.py
+++ hyperdock/worker/worker.py
@@ -4,6 +4,8 @@ from time import sleep
 import secrets
 from datetime import datetime
 
+from ..common.workqueue import WorkQueue
+from ..common.experiment import Experiment
 
 SLEEP_TIME = 5
 
@@ -17,6 +19,9 @@ class Worker(Thread):
         self.id = self._generate_id()
         self.logger = logging.getLogger('Worker %s' % self.id)
         self._running = True
+        self.workqueue = WorkQueue(mongodb)
+        self.experiments = []
+        self.max_experiments = 1
 
     def run(self):
         """
@@ -58,21 +63,46 @@ class Worker(Thread):
         """
         Monitor the status of the current experiments.
         """
-        pass
+        exps = []
+        for ex in self.experiments:
+            if ex.is_running():
+                # Update the stay alive
+                self.workqueue.update_job(ex.id)
+                exps.append(ex)
+            else:
+                # Wrap up the finished experiment
+                self._cleanup_experiment(ex)
+        self.experiments = exps
 
-    def _start_new_experiments(self):
+    def _cleanup_experiment(self, experiment):
+        """
+        Takes an ended experiment and updates the work queue.
+        """
+        res = experiment.get_result()
+        experiment.cleanup()
+        self.workqueue.finish_job(experiment.id, res)
+
+    def _start_new_experiments(self, experiment_cls=Experiment):
         """
         If not the max concurrent number of experiments is reached,
         starta new experiment.
         """
-        pass
+        for i in range(self.max_experiments - len(self.experiments)):
+            job = self.workqueue.assign_next_job(self.id)
+            experiment = experiment_cls(job)
+            experiment.start()
+            self.experiments.append(experiment)
 
     def _shutdown(self):
         """
         Performs an immediate but graceful shutdown of the worker and
         experiments.
         """
-        pass
+        for ex in self.experiments:
+            ex.stop()
+            res = ex.get_result()
+            self.workqueue.finish_job(ex.id, res)
+        self.experiments = []
 
     def _generate_id(self):
         """
