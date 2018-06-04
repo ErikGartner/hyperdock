5ae43678ebf24509d08f55d342b841e83d3b083e
johanahlqvist <johan_ahlkvist@hotmail.com>
2018-06-04 15:32:03 +0200

    Add form for submitting trials

--- web/imports/startup/server/register-api.js
+++ web/imports/startup/server/register-api.js
@@ -1,4 +1,5 @@
 // Register your apis here
 import '../../api/workers/server/publications.js';
 import '../../api/trialqueue/server/publications.js';
+import '../../api/trialqueue/methods.js';
 import '../../api/workqueue/server/publications.js';
--- web/imports/ui/components/trialqueue/trialqueue.js
+++ web/imports/ui/components/trialqueue/trialqueue.js
@@ -7,3 +7,22 @@ Template.trialqueue.helpers({
     return TrialQueue.find({});
   },
 });
+
+Template.trialqueue.events({
+  'submit .add-trial'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const docker_image = target.docker_image;
+    const param_space = target.param_space;
+
+    Meteor.call('trialqueue.insert', docker_image.value, param_space.value, (error) => {
+      if (error) {
+        alert(error.error);
+      } else {
+        docker_image.value = '';
+        param_space.value = '';
+      }
+    });
+  },
+});
