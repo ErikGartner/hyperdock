7b50c7eb151be4a1a1a629be88533e98c005eeac
Erik Gärtner <erik@gartner.io>
2018-06-05 13:59:46 +0200

    Add experiment class

--- hyperdock/common/experiment.py
+++ hyperdock/common/experiment.py
@@ -1,6 +1,11 @@
 import logging
+from datetime import datetime
+import os
+import json
 
 import docker
+
+from .utils import try_key
 
 
 class Experiment:
@@ -14,64 +19,174 @@ class Experiment:
         self._queue_job = job
         self.id = job['_id']
         self.logger = logging.getLogger('Experiment %s' % self.id)
+        self._is_running = False
+        self._result = {}
+        self._container = None
+        self._volumes = []
+        self._volume_root = None
+        self._docker_client = docker.from_env()
+        self.has_started = False
 
     def start(self):
         """
-        Starts the experiment.
+        Start the experiment.
         """
-        image = self._queue_job['data']['docker']['image']
-        container = self._start_container(image)
-        if container is None:
-            pass
+        if self.has_started:
+            raise RuntimeError('This experiment has already been executed.')
+        self.has_started = True
 
+        self._is_running = True
+        self._setup_volumes()
+        image = self._queue_job['data']['docker']['image']
+        self._container = self._start_container(image)
+
+        if self._container is None:
+            self._is_running = False
 
     def stop(self):
         """
         Stops the experiment.
         """
-        pass
+        if self._is_running and self._container is not None:
+            try:
+                self._container.stop()
+            except docker.errors.APIError as e:
+                self.logger.error('Failed to stop container:\n%s' % e)
+                self._result = {'status': 'fail', 'msg': e}
 
     def cleanup(self):
         """
         Stops and removes temporary files.
         """
-        pass
+        self.stop()
+        if self._container is not None:
+            try:
+                self._container.remove()
+            except docker.errors.APIError as e:
+                self.logger.error('Failed to remove container:\n%s' % e)
+            self._container = None
 
     def is_running(self):
         """
         Checks with underlying runner if the current experiment is running.
         Otherwise it could: not be started, finished or failed.
         """
-        pass
+        if self._is_running:
+            self._is_running = self._check_container_running()
+
+            if not self._is_running:
+                # Container finished since last check, fetch the result.
+                self._fetch_result()
+
+        return self._is_running
 
     def get_result(self):
         """
         Returns the result, that is: state, loss, and any extra data.
         """
-        pass
+        if self.is_running():
+            return {'state': 'running'}
+        else:
+            return self._result
 
     def _start_container(self, image):
         """
         Starts a docker container and returns its handle.
         """
-
         try:
-            container = self.docker_client.containers.run(
+            container = self._docker_client.containers.run(
                 image=image,
                 tty=False,
                 detach=True,
-                environment=json.loads({}),
-                runtime='',
+                environment=try_key(self._queue_job['data'], {}, 'docker', 'environment'),
+                runtime=try_key(self._queue_job['data'], '', 'docker', 'runtime'),
                 log_config={'type': 'json-file'},
                 stdout=True,
                 stderr=True,
-                volumes=[])
+                volumes=self._volumes
+            )
             return container
 
-        except (docker.errors.ContainerError, docker.errors.APIError,
+        except (docker.errors.ContainerError,
+                docker.errors.APIError,
                 docker.errors.ImageNotFound) as e:
-            self.logger.error(e)
+            self.logger.error('Failed to start container:\n%s' % e)
+            self._result = {'status': 'fail', 'msg': e}
             return None
+
+    def _check_container_running(self):
+        """
+        Checks if the container is running or not.
+        """
+        if self._container is None:
+            return False
+        else:
+            self._container.reload()
+            return self._container.status == 'running'
+
+    def _fetch_result(self):
+        """
+        After a container has finished. This method fetches the
+        result from the container.
+        """
+        if self._container is None:
+            self._result = {'state': 'fail'}
+
+        else:
+            docker_logs = self._container.logs(stdout=True, stderr=True)
+            try:
+                with open(os.path.join(self._volume_root, 'docker_log.txt'), 'ab') as f:
+                    f.write(docker_logs)
+            except:
+                pass
+
+            try:
+                with open(os.path.join(self._volume_root, 'loss.json'), 'r') as f:
+                    loss = json.load(f)
+                    self._result = loss
+
+            except:
+                self.logger.error('Failed to read loss')
+                self._result = {'state': 'fail'}
+
+    def _setup_volumes(self):
+        """
+        Sets up the volumes on the host's end along with the paths for the
+        volumes.
+        """
+        data = self._queue_job['data']
+
+        # Results folder path on host
+        results_folder = try_key(data, 'results', 'volumes', 'results')
+        folder_name = 'run_%s' % datetime.utcnow().strftime('%Y-%m-%d_%H.%M.%S.%f')
+        volume_root = os.path.join(results_folder, folder_name)
+        os.makedirs(volume_root, exist_ok=True)
+
+        # Folder paths
+        out_folder = os.path.join(volume_root, 'out')
+        in_file = os.path.join(volume_root, 'params.json')
+        loss_file = os.path.join(volume_root, 'loss.json')
+        docker_log = os.path.join(volume_root, 'docker_log.txt')
+
+        # Make empty foldes and files
+        os.mkdir(out_folder)
+        open(loss_file, 'a').close()
+
+        # Create params file
+        with open(in_file, 'w') as f:
+            json.dump(self._queue_job['parameters'], f)
+
+        self._volumes = [
+            '%s:/hyperdock' % volume_root,
+        ]
+        self._volume_root = volume_root
+
+        # Get host data folder
+        host_data_folder = try_key(data, '', 'volumes', 'data')
+        if host_data_folder is not '':
+            self._volumes.append('%s:/data:ro' % host_data_folder)
+
+        return self._volumes
 
     def __str__(self):
         return 'Experiment: %s' % self._queue_job
@@ -104,3 +219,6 @@ class MockExperiment(Experiment):
     def get_result(self):
         state = 'ok' if not self.failure else 'failure'
         return {'loss': 1.0, 'state': state}
+
+    def cleanup(self):
+        pass
---
+++ tests/common/test_experiment.py
@@ -0,0 +1,118 @@
+from unittest import TestCase, skip
+from datetime import datetime, timedelta
+from time import sleep
+from tempfile import mkdtemp
+import shutil
+import os
+import json
+
+import docker
+
+from hyperdock.common.experiment import Experiment
+
+
+#@skip("Skip on Travis since it requires a Docker installation")
+class TestExperiment(TestCase):
+
+    def setUp(self):
+        self.docker = docker.from_env()
+        self.image = 'erikgartner/hyperdock-test:latest'
+        self.test_folder = mkdtemp(prefix='hyperdock-unittest-result-folder')
+
+        self.job = {
+            'created_on': datetime.utcnow(),
+            'start_time': datetime.utcnow(),
+            'end_time': -1,
+            'data': {
+                'docker': {
+                    'image': self.image,
+                    'environment': [
+                        'NVIDIA_VISIBLE_DEVICES=1'
+                    ],
+                },
+                'volumes': {
+                    'results': self.test_folder,
+                }
+            },
+            'parameters': {
+                'a': 1,
+                'b': 2,
+            },
+            'worker': 'worker-1',
+            'last_update': -1,
+            'priority': 1,
+            'result': {},
+            '_id': 'job-1',
+        }
+
+        self.experiment = Experiment(self.job)
+        self.container = None
+
+    def tearDown(self):
+        if self.container is not None:
+            self.container.remove(force=True)
+        shutil.rmtree(self.test_folder)
+
+    def test_start_container(self):
+        self.container = self.experiment._start_container(self.image)
+        self.assertIsNotNone(self.container, 'Error starting container')
+
+        self.container.wait(timeout=10)
+        self.container.reload()
+        self.assertEqual(self.container.status, 'exited',
+                         'Container had not stopped')
+
+    def test_start(self):
+        self.experiment.start()
+        self.container = self.experiment._container
+
+        # Make sure it has started. Not that if it exits quickly we might miss
+        # it.
+        self.assertTrue(self.experiment.is_running(), 'Container didnt start')
+
+        # Ensure correct image
+        self.assertTrue(self.image in str(self.experiment._container.image),
+                        'Incorrect image')
+
+        self.assertTrue(self.experiment._volume_root.startswith(self.test_folder),
+                        'Incorrect result folder')
+
+        for e in self.job['data']['docker']['environment']:
+            self.assertIn(e,
+                          self.container.attrs['Config']['Env'],
+                          'Missing env from container')
+
+        # Wait for container to exit
+        self.container.wait(timeout=10)
+
+        # Make sure it says it has has stopped
+        self.assertFalse(self.experiment.is_running(), 'Container has stopped')
+
+
+        # Check parameters were correct
+        with open(os.path.join(self.experiment._volume_root,
+                               'params.json'), 'r') as f:
+            params = json.load(f)
+            self.assertDictEqual(params, self.job['parameters'],
+                                 'Incorrect parameters!')
+
+
+        # Get result, ensure it is correct
+        result = self.experiment.get_result()
+        self.assertDictEqual(result, {'state': 'ok', 'loss': 0})
+
+        # Ensure the container still exists in a stopped format
+        self.container.reload()
+        self.assertEqual(self.container.status, 'exited',
+                         'Container had not stopped')
+
+        # Clean up and make sure it is removed
+        self.experiment.cleanup()
+        with self.assertRaises(docker.errors.NotFound):
+            self.container.reload()
+        self.container = None
+
+        self.assertIsNone(self.experiment._container)
+
+        with self.assertRaises(RuntimeError):
+            self.experiment.start()
