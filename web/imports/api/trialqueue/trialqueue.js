dc2c834c5fa0c4128f7b8e7bb094bf2027a3fc54
Erik Gärtner <erik@gartner.io>
2018-06-05 17:08:48 +0200

    Update web interface to add work

--- web/imports/api/trialqueue/trialqueue.js
+++ web/imports/api/trialqueue/trialqueue.js
@@ -1,3 +1,67 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 export const TrialQueue = new Mongo.Collection('trialqueue');
+
+TrialSchema = new SimpleSchema({
+    start_time: {
+      type: SimpleSchema.Integer,
+      autoValue: function() {
+        if (this.isInsert) {
+          return -1;
+        }
+      }
+    },
+    end_time: {
+      type: SimpleSchema.Integer,
+      autoValue: function() {
+        if (this.isInsert) {
+          return -1;
+        }
+      }
+    },
+    created_on: {
+      type: Date,
+      autoValue: function() {
+        if (this.isInsert) {
+          return new Date();
+        } else {
+          this.unset();  // Prevent user from supplying their own value
+        }
+      }
+    },
+    priority: {
+      type: SimpleSchema.Integer,
+      defaultValue: -1
+    },
+    param_space: {
+      type: Object,
+      blackbox: true
+    },
+    data: {
+      type: Object,
+      blackbox: true
+    }
+});
+TrialQueue.attachSchema(TrialSchema);
+
+TrialQueue.permit(['insert', 'update', 'remove']).allowInClientCode();
+
+export const TrialInsertSchema = new SimpleSchema({
+  docker_image: {
+    label: 'Docker Image',
+    type: String,
+  },
+  results_path: {
+    type: String,
+    label: 'Results Directory',
+  },
+  data_path: {
+    type: String,
+    label: 'Data Directory',
+  },
+  param_space: {
+    label: 'Parameter Space',
+    type: String,
+  }
+});
--- web/imports/startup/server/register-api.js
+++ web/imports/startup/server/register-api.js
@@ -1,5 +1,4 @@
 // Register your apis here
 import '../../api/workers/server/publications.js';
 import '../../api/trialqueue/server/publications.js';
-import '../../api/trialqueue/methods.js';
 import '../../api/workqueue/server/publications.js';
