8146c65c55c369985b0a70ebb824aad1313c6bf8
Erik Gärtner <erik@gartner.io>
2018-06-08 16:31:28 +0200

    Made supervisor and worker launchable through cli commands #5

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 from setuptools import setup, find_packages
 from hyperdock import __version__
 
-tests_require = [
-    'mock',
-    'nose',
-    'coverage',
-    'yanc',
-    'preggy',
-    'tox',
-    'ipdb',
-    'coveralls',
-    'sphinx',
-]
-
 setup(
     name='hyperdock',
     version=__version__,
-    description='A hyperoptimizer living in Docker',
-    long_description='''A hyperoptimizer living in Docker''',
-    keywords='',
+    description='A simple program for distributed hyperparameter optimization in Docker',
+    long_description='''A simple program for distributed hyperparameter optimization in Docker''',
+    keywords='hyperparameter optimization,docker',
     author='Erik Gärtner',
     author_email='erik@gartner.io',
     url='http://github.com/ErikGartner/hyperdock',
-    license='',
+    license='Apache 2.0',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -35,17 +23,41 @@ setup(
     packages=find_packages(),
     include_package_data=False,
     install_requires=[
-        # add your dependencies here
-        # remember to use 'package-name>=x.y.z,<x.y+1.0' notation (this way you get bugfixes)
+        'certifi==2018.4.16',
+        'chardet==3.0.4',
+        'click==6.7',
+        'decorator==4.3.0',
+        'dill==0.2.7.1',
+        'docker-pycreds==0.2.3',
+        'docker==3.3.0',
+        'future==0.16.0',
+        'hyperopt==0.1',
+        'idna==2.6',
+        'mock==2.0.0',
+        'mongomock==3.10.0',
+        'networkx==1.11',
+        'nose==1.3.7',
+        'numpy==1.14.3',
+        'pbr==4.0.3',
+        'pymongo==3.6.1',
+        'requests==2.18.4',
+        'scikit-learn==0.19.1',
+        'scipy==1.1.0',
+        'sentinels==1.0.0',
+        'six==1.11.0',
+        'sklearn==0.0',
+        'urllib3==1.22',
+        'websocket-client==0.48.0',
     ],
     extras_require={
-        'tests': tests_require,
+        'tests': [
+            'nose',
+        ],
     },
     entry_points={
         'console_scripts': [
-            # add cli scripts here in this form:
-            # 'hyperdock=hyperdock.cli:main',
-            'hyperdock-supervisor=hyperdock.supervisor.cli'
+            'hyperdock-supervisor = hyperdock.supervisor.main:launch_supervisor',
+            'hyperdock-worker = hyperdock.worker.main:launch_worker',
         ],
     },
 )
