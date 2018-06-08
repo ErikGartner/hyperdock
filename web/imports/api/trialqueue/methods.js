a22f020350703d63f0c1a2fd11dd3e987d7676fe
Erik Gärtner <erik@gartner.io>
2018-06-08 12:13:43 +0200

    Add cancelling of trials

---
+++ web/imports/api/trialqueue/methods.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { TrialQueue } from './trialqueue.js';
+import { WorkQueue } from '../workqueue/workqueue.js';
+
+Meteor.methods({
+  'trialqueue.cancel'(id) {
+    check(id, String);
+
+    let t = TrialQueue.findOne({_id: id, end_time: -1});
+    if (t == null) {
+      throw new Meteor.Error('trialqueue.cancel called with invalid id.');
+    }
+
+    let works = WorkQueue.find({trial: id, end_time: -1, cancelled: false});
+    works.forEach(function (w){
+      w.cancelled = true;
+      w.end_time = new Date();
+      if (w.start_time == -1) {
+        w.start_time = w.end_time;
+      }
+      WorkQueue.update(w._id, {$set: {cancelled: w.cancelled,
+                                      end_time: w.end_time,
+                                      start_time: w.start_time}});
+    });
+
+  }
+});
--- web/imports/startup/server/register-api.js
+++ web/imports/startup/server/register-api.js
@@ -2,3 +2,6 @@
 import '../../api/workers/server/publications.js';
 import '../../api/trialqueue/server/publications.js';
 import '../../api/workqueue/server/publications.js';
+
+import '../../api/trialqueue/methods.js';
+import '../../api/workqueue/methods.js';
