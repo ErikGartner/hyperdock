bc9705f8989ac8a0ee13e352611b92a1b05acdb8
Erik Gärtner <erik@gartner.io>
2018-06-08 11:15:02 +0200

    Add cancel to workqueue

--- web/imports/startup/client/global_helpers.js
+++ web/imports/startup/client/global_helpers.js
@@ -1,3 +1,7 @@
 Template.registerHelper('json2string', (j) => {
   return JSON.stringify(j, null, '  ');
 });
+
+Template.registerHelper('niceTime', (t) => {
+  return moment(t).format('MMMM Do hh:mm:ss');
+});
