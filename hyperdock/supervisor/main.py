427eda936ed77b2e3d869e2e46a5373ae0d371f2
Erik Gärtner <erik@gartner.io>
2018-06-10 20:25:11 +0200

    Fix the logging format and level #13

--- hyperdock/supervisor/main.py
+++ hyperdock/supervisor/main.py
@@ -4,11 +4,13 @@ import click
 from pymongo import MongoClient
 
 from .supervisor import Supervisor
+from ..common import utils
 
 
 @click.command()
 @click.option('--mongodb', default='mongodb://localhost:27017/hyperdock', help='The URI to the MongoDB.')
 def launch_supervisor(mongodb):
+    utils.setup_logging()
     database = MongoClient(mongodb).get_default_database()
     supervisor = Supervisor(database)
     supervisor.start()
