2b9249f2cd77f27fb4761aae81ffd747f1006d0b
Erik Gärtner <erik@gartner.io>
2018-06-11 12:32:18 +0200

    Push to Docker hub. Old images now tagged as :legacy

--- hyperdock/version.py
+++ hyperdock/version.py
@@ -1,1 +1,5 @@
 __version__ = '0.4.1'  # NOQA
+
+
+if __name__ == '__main__':
+    print(__version__)
--- push_image.sh
+++ push_image.sh
@@ -1,6 +1,15 @@
-docker build -t erikgartner/hyperdock-webui:latest -f docker/Dockerfile.webui web/
-docker build -t erikgartner/hyperdock-supervisor:latest -f docker/Dockerfile.supervisor .
-docker build -t erikgartner/hyperdock-worker:latest -f docker/Dockerfile.worker .
-docker push erikgartner/hyperdock-supervisor
-docker push erikgartner/hyperdock-worker
-docker push erikgartner/hyperdock-webui
+VERSION=$(python hyperdock/version.py)
+echo "Building images and pushing for version $VERSION"
+
+docker build -t erikgartner/hyperdock-webui:$VERSION -f docker/Dockerfile.webui web/
+docker build -t erikgartner/hyperdock-supervisor:$VERSION -f docker/Dockerfile.supervisor .
+docker build -t erikgartner/hyperdock-worker:$VERSION -f docker/Dockerfile.worker .
+docker tag erikgartner/hyperdock-supervisor:$VERSION erikgartner/hyperdock-supervisor:latest
+docker tag erikgartner/hyperdock-worker:$VERSION erikgartner/hyperdock-worker:latest
+docker tag erikgartner/hyperdock-webui:$VERSION erikgartner/hyperdock-webui:latest
+docker push erikgartner/hyperdock-supervisor:$VERSION
+docker push erikgartner/hyperdock-worker:$VERSION
+docker push erikgartner/hyperdock-webui:$VERSION
+docker push erikgartner/hyperdock-supervisor:latest
+docker push erikgartner/hyperdock-worker:latest
+docker push erikgartner/hyperdock-webui:latest
