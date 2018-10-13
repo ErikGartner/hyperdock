3b1cd959bfb7cd390e66326f7b89fcef345e2a48
Erik Gärtner <erik@gartner.io>
2018-10-13 10:48:47 +0200

    Fix typo

--- web/imports/api/workqueue/server/publications.js
+++ web/imports/api/workqueue/server/publications.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { WorkQueue } from '../workqueue.js';
 
 Meteor.publish('workqueue.all.limited', function () {
-  return WorkQueue.find({}, { fields: { update: 0 , parameters: 0});
+  return WorkQueue.find({}, { fields: { update: 0, parameters: 0 }});
 });
